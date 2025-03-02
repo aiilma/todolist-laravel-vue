9a54c03e5644ada1facd211fe60abfab842f019c
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-02 18:13:48 +0300

    chore(frontend * package.json): +toastify

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import 'vue3-toastify/dist/index.css';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
