2a4c1209aeb77996cd9b2e7edfa57b59b277c610
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-02 18:20:33 +0300

    feat(frontend * stores): pinia connected

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import 'vue3-toastify/dist/index.css';
 import './style.css'
 import App from './App.vue'
+import {createPinia} from "pinia";
 import router from "./router";
+
+const pinia = createPinia()
 
 const app = createApp(App)
     .use(router)
+    .use(pinia)
 
 app.mount('#app')
