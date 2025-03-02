f63293495da9e8abad6f63179668f8a3446664ef
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-02 20:16:24 +0300

    feat(frontend * auth): гварды для авторизованных и гостевых пользователей

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -2,22 +2,26 @@ import {createRouter, createWebHistory} from "vue-router";
 import TasksPage from "../views/TasksPage.vue";
 import RegisterPage from "../views/RegisterPage.vue";
 import LoginPage from "../views/LoginPage.vue";
+import {useAuthStore} from "../stores/useAuthStore.ts";
 
 const routes = [
     {
         path: '/',
         name: 'TasksPage',
-        component: TasksPage
+        component: TasksPage,
+        meta: { requiresAuth: true }
     },
     {
         path: '/register',
         name: 'RegisterPage',
-        component: RegisterPage
+        component: RegisterPage,
+        meta: { requiresGuest: true }
     },
     {
         path: '/login',
         name: 'LoginPage',
-        component: LoginPage
+        component: LoginPage,
+        meta: { requiresGuest: true }
     },
 ]
 
@@ -25,5 +29,16 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeEach((to, _, next) => {
+    const authStore = useAuthStore();
+    if (to.meta.requiresAuth && !authStore.token) {
+        next({ name: 'LoginPage' });
+    } else if (to.meta.requiresGuest && authStore.token) {
+        next({ name: 'TasksPage' });
+    } else {
+        next();
+    }
+});
 
 export default router
