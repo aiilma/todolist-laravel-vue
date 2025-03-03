68b26356a72c29a6bc8e3183c19601536a6fe11b
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-03 08:55:32 +0300

    feat(frontend): page for tasks; UserLayout.vue

--- src/frontend/src/views/TasksPage.vue
+++ src/frontend/src/views/TasksPage.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {onMounted, ref, watch} from "vue";
+import UserLayout from "../components/layout/UserLayout.vue";
+import TasksFilters from "../components/TasksFilters.vue";
+import TasksLayout from "../components/layout/TasksLayout.vue";
+import TaskList from "../components/TaskList.vue";
+import {storeToRefs} from "pinia";
+import {useTaskStore} from "../stores/useTaskStore.ts";
+
+const taskStore = useTaskStore();
+
+const {totalTasksCount} = storeToRefs(taskStore);
+const showFilters = ref(false);
+
+watch(totalTasksCount, (newCount) => showFilters.value = newCount > 0);
+onMounted(() => showFilters.value = totalTasksCount.value > 0);
+</script>
 
 <template>
-  <div>
-    <h1>Tasks Page</h1>
-  </div>
+  <UserLayout>
+    <TasksLayout title="Tasks Page">
+      <template #extra v-if="showFilters">
+        <hr class="mt-4" >
+        <TasksFilters/>
+      </template>
+
+      <TaskList/>
+    </TasksLayout>
+  </UserLayout>
 </template>
